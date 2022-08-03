6bceb2287467c7c2230d35f009a423cd8bb8467b
LucianoReiis <luciano.reis04@hotmail.com>
2022-08-03 19:26:08 -0300

    Inclusão da pagina login e criação do formularios.

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
+from forms import FormLogin, FormCriarConta
 
 app = Flask(__name__)
 
 lista_usuarios = ['Lira', 'Alon', '...']
+
+app.config['SECRET_KEY'] = 'fcda296a5bb64c9959ffe7bcf90fc253'
 
 @app.route("/")
 def home():
@@ -15,6 +18,12 @@ def contato():
 @app.route('/usuarios')
 def usuarios():
     return render_template('usuarios.html', lista_usuarios=lista_usuarios)
+
+@app.route('/login')
+def login():
+    form_login = FormLogin()
+    form_criarconta = FormCriarConta()
+    return render_template('login.html', form_login=form_login, form_criarconta=form_criarconta)
 
 
 if __name__ == '__main__':
