525c3995b8767017be1c893d6c7af31fbfb0e710
LucianoReiis <luciano.reis04@hotmail.com>
2022-08-15 07:30:35 -0300

    Correção do banco de dados

--- models.py
+++ models.py
@@ -5,10 +5,10 @@ from datetime import datetime
 
 
 class Usuario(database.Model):
-    id = database.column(database.Integer, primary_key=True)
-    username = database.column(database.String, nullable=False)
-    email = database.column(database.String, nullable=False, unique=True)
-    senha = database.column(database.String, nullable=False)
+    id = database.Column(database.Integer, primary_key=True)
+    username = database.Column(database.String, nullable=False)
+    email = database.Column(database.String, nullable=False, unique=True)
+    senha = database.Column(database.String, nullable=False)
     foto_perfil = database.Column(database.String, default='default.jpg')
     posts = database.relationship('Post', backref='autor', lazy=True)
     cursos = database.Column(database.String, nullable=False, default='Não informado')
@@ -16,7 +16,7 @@ class Usuario(database.Model):
 
 
 class Post(database.Model):
-    id = database.column(database.Integer, primary_key=True)
+    id = database.Column(database.Integer, primary_key=True)
     titulo = database.Column(database.String, nullable=False)
     corpo = database.Column(database.Text, nullable=False)
     data_criacao = database.Column(database.DateTime, nullable=False, default=datetime.utcnow)
