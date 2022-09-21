9210eebc2fd9edf5f7018f772c2e3389b13645a9
LucianoReiis <luciano.reis04@hotmail.com>
2022-09-21 20:01:55 -0300

    validação de e-mail

--- comunidadeimpressionadora/forms.py
+++ comunidadeimpressionadora/forms.py
@@ -1,6 +1,8 @@
+from xml.dom import ValidationErr
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from comunidadeimpressionadora.models import Usuario
 
 class FormCriarConta(FlaskForm):
     username = StringField('Nome de Usúario', validators=[ DataRequired()])
@@ -8,6 +10,14 @@ class FormCriarConta(FlaskForm):
     senha = PasswordField('Senha', validators=[DataRequired(), Length(6, 20)])
     confirmacao_senha = PasswordField('Confirmação de Senha', validators=[DataRequired(), EqualTo('senha')])
     botao_submit_criarconta = SubmitField('Criar Conta')
+
+    def validate_email(self, email):
+        usuario = Usuario.query.filter_by(email=email.data).first()
+        if usuario:
+            raise ValidationError('E-mail já cadastrado. Cadastre-se com outro e-mail ou faça login para continuar')
+
+
+
 
 class FormLogin(FlaskForm):
     email =  StringField('E-mail', validators=[DataRequired(), Email()])
