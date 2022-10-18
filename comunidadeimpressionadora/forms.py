89c2c6dab1ef04dc91be587d77fe3c30b771a56e
LucianoReiis <luciano.reis04@hotmail.com>
2022-10-18 20:33:26 -0300

    Pagina perfil e CSS

--- comunidadeimpressionadora/forms.py
+++ comunidadeimpressionadora/forms.py
@@ -1,14 +1,14 @@
-from xml.dom import ValidationErr
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from comunidadeimpressionadora.models import Usuario
 
+
 class FormCriarConta(FlaskForm):
-    username = StringField('Nome de Usúario', validators=[ DataRequired()])
-    email =  StringField('E-mail', validators=[DataRequired(), Email()])
+    username = StringField('Nome de Usuário', validators=[DataRequired()])
+    email = StringField('E-mail', validators=[DataRequired(), Email()])
     senha = PasswordField('Senha', validators=[DataRequired(), Length(6, 20)])
-    confirmacao_senha = PasswordField('Confirmação de Senha', validators=[DataRequired(), EqualTo('senha')])
+    confirmacao_senha = PasswordField('Confirmação da Senha', validators=[DataRequired(), EqualTo('senha')])
     botao_submit_criarconta = SubmitField('Criar Conta')
 
     def validate_email(self, email):
@@ -17,10 +17,8 @@ class FormCriarConta(FlaskForm):
             raise ValidationError('E-mail já cadastrado. Cadastre-se com outro e-mail ou faça login para continuar')
 
 
-
-
 class FormLogin(FlaskForm):
-    email =  StringField('E-mail', validators=[DataRequired(), Email()])
+    email = StringField('E-mail', validators=[DataRequired(), Email()])
     senha = PasswordField('Senha', validators=[DataRequired(), Length(6, 20)])
     lembrar_dados = BooleanField('Lembrar Dados de Acesso')
     botao_submit_login = SubmitField('Fazer Login')
--- comunidadeimpressionadora/routes.py
+++ comunidadeimpressionadora/routes.py
@@ -1,24 +1,27 @@
-import email
-from flask import render_template, redirect, url_for, request, flash
+from flask import render_template, redirect, url_for, flash, request
 from comunidadeimpressionadora import app, database, bcrypt
 from comunidadeimpressionadora.forms import FormLogin, FormCriarConta
 from comunidadeimpressionadora.models import Usuario
-from flask_login import login_user, logout_user, current_user
+from flask_login import login_user, logout_user, current_user, login_required
+
+lista_usuarios = ['Lira', 'João', 'Alon', 'Alessandra', 'Amanda']
 
 
-lista_usuarios = ['Lira', 'Alon', '...']
-
-@app.route("/")
+@app.route('/')
 def home():
     return render_template('home.html')
 
-@app.route("/contato")
+
+@app.route('/contato')
 def contato():
     return render_template('contato.html')
 
+
 @app.route('/usuarios')
+@login_required
 def usuarios():
     return render_template('usuarios.html', lista_usuarios=lista_usuarios)
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -35,28 +38,32 @@ def login():
             else:
                 return redirect(url_for('home'))
         else:
-            flash(f'Falha no login. E-mail ou Senha Incorretos', 'alert-danger')
+            flash(f'Falha no Login. E-mail ou Senha Incorretos', 'alert-danger')
     if form_criarconta.validate_on_submit() and 'botao_submit_criarconta' in request.form:
         senha_cript = bcrypt.generate_password_hash(form_criarconta.senha.data)
         usuario = Usuario(username=form_criarconta.username.data, email=form_criarconta.email.data, senha=senha_cript)
         database.session.add(usuario)
         database.session.commit()
-        #criou conta com sucesso.
         flash(f'Conta criada para o e-mail: {form_criarconta.email.data}', 'alert-success')
         return redirect(url_for('home'))
     return render_template('login.html', form_login=form_login, form_criarconta=form_criarconta)
 
 
-    @app.route('/sair')
-    def sair():
-        logout_user()
-        flash(f'Logout feito com sucesso!', 'alert-success')
-        return redirect(url_for('home'))
+@app.route('/sair')
+@login_required
+def sair():
+    logout_user()
+    flash(f'Logout Feito com Sucesso', 'alert-success')
+    return redirect(url_for('home'))
 
-    @app.route('/perfil')
-    def perfil():
-        return render_template('perfil.html')
 
-    @app.route('/post/criar')
-    def criar_post():
-        return render_template('criarpost.html')
+@app.route('/perfil')
+@login_required
+def perfil():
+    return render_template('perfil.html')
+
+
+@app.route('/post/criar')
+@login_required
+def criar_post():
+    return render_template('criarpost.html')
