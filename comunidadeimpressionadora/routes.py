1694aa413eed3d2a377ff814c528b3f58fa5e332
LucianoReiis <luciano.reis04@hotmail.com>
2022-12-21 18:25:36 -0300

    Editar perfil Cursos

--- comunidadeimpressionadora/routes.py
+++ comunidadeimpressionadora/routes.py
@@ -85,6 +85,16 @@ def salvar_imagem(imagem):
     return nome_arquivo
 
 
+def atualizar_cursos(form):
+    lista_cursos = []
+    for campo in form:
+        if 'curso_' in  campo.name:
+            if campo.data:
+                lista_cursos.append(campo.label.text)
+    return ';'.join(lista_cursos)
+
+
+
 @app.route('/perfil/editar', methods=['GET', 'POST'])
 @login_required
 def editar_perfil():
@@ -95,6 +105,7 @@ def editar_perfil():
         if form.foto_perfil.data:
             nome_imagem = salvar_imagem(form.foto_perfil.data)
             current_user.foto_perfil = nome_imagem
+        current_user.cursos = atualizar_cursos(form)
         database.session.commit()
         flash('Perfil atualizado com Sucesso', 'alert-success')
         return redirect(url_for('perfil'))
